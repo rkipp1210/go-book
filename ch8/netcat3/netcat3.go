653f135b002c800f691cc8a5c763db2a24c3b912
Ross K <rkippenbrock@gmail.com>
2016-07-21 11:52:44 -0400

    using channels in netcat to keep connection open

---
+++ ch8/netcat3/netcat3.go
@@ -0,0 +1,30 @@
+// Read only TCP client using channels
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		io.Copy(os.Stdout, conn) //ignoring errors
+		log.Println("done")
+		done <- struct{}{} // signal to main goroutine
+	}() // literal function
+	mustCopy(conn, os.Stdin)
+	<-done // wait for the background goroutine to finish
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
